517245d101825b78f7f45643c29c57409c39aa41
agilob <info@agilob.net>
2020-09-07 11:54:08 +0100

    Add sql server to openhack network

--- src/sqlserver.sh
+++ src/sqlserver.sh
@@ -1,5 +1,5 @@
 docker network create openhack
 
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=OpenHack12345' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+docker run --network openhack -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=OpenHack12345' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
 
 docker run --network openhack -e SQLFQDN=localhost -e SQLUSER=SA -e SQLPASS=OpenHack12345 -e SQLDB=mydrivingDB -d openhack/data-load:v1
