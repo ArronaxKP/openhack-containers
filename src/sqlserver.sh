7faffcd5969d2c3b766095c3beea5e8ad6145665
agilob <info@agilob.net>
2020-09-07 11:52:41 +0100

    Add sqlserver container starter

--- src/sqlserver.sh
+++ src/sqlserver.sh
@@ -1,3 +1,5 @@
 docker network create openhack
 
-docker run --network openhack -e SQLFQDN=localhost -e SQLUSER=openhack -e SQLPASS=openhack -e SQLDB=mydrivingDB openhack/data-load:v1
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=OpenHack12345' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+
+docker run --network openhack -e SQLFQDN=localhost -e SQLUSER=SA -e SQLPASS=OpenHack12345 -e SQLDB=mydrivingDB -d openhack/data-load:v1
